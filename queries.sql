462d45b59d744fb2c93ece04478c15651ba77564
Olaoye Oluwatoyin Abayomi <mindprepopps@gmail.com>
2022-12-21 10:53:18 +0100

    Query data with appropriate data for classification

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * from animals WHERE name LIKE '%mon';
+SELECT * from animals WHERE date_of_birth  BETWEEN '2016-01-01' and '2019-12-31';
+SELECT * from ANIMALS WHERE neutered=true and escape_attempts<3;
+select date_of_birth from animals where name='Agumon' or name='Pikachu';
+select (name, escape_attempts) from animals where weight_kg>10.5;
+select * from animals where neutered=true;
+select * from animals WHERE NOT name='Gabumon';
+select * from animals where weight_kg>=10.4 or weight_kg>=17.3;

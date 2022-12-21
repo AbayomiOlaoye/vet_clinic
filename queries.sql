6687cb35923f88c238a4618ec8ca15b752351d5f
Olaoye Oluwatoyin Abayomi <mindprepopps@gmail.com>
2022-12-21 12:40:42 +0100

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 
 SELECT * from animals WHERE name LIKE '%mon';
 SELECT name from animals WHERE date_of_birth  BETWEEN '2016-01-01' and '2019-12-31';
-SELECT * from ANIMALS WHERE neutered=true and escape_attempts<3;
+SELECT name from ANIMALS WHERE neutered=true and escape_attempts<3;
 select date_of_birth from animals where name='Agumon' or name='Pikachu';
 select (name, escape_attempts) from animals where weight_kg>10.5;
 select * from animals where neutered=true;
